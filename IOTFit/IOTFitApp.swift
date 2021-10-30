7800ce4dbb7d381b456fac5b3a219df39fa4ae28
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-10-30 08:34:50 +0200

    Initial Commit

---
+++ IOTFit/IOTFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOTFitApp.swift
+//  IOTFit
+//
+//  Created by Amr Hesham on 30/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct IOTFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
